4e9a55461e8866c2df32ea42f257c6b1b2242410
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2021-12-01 09:58:03 +0100

    Create monmodule.c

---
+++ monmodule.c
@@ -0,0 +1,18 @@
+// Nom : monmodule.c
+// Sujet: Module de noyau Linux
+// Formation
+#include <linux/module.h>
+#include <linux/kernel.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Stagiaire");
+MODULE_DESCRIPTION("Module de noyau Linux");
+MODULE_VERSION("Version 1");
+int init_module(void)
+{
+printk(KERN_INFO "Le module est chargé.€n");
+return(0);
+}
+void cleanup_module(void)
+{
+printk(KERN_INFO "Le module est déchargé.€n");
+}
