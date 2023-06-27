d2e4b46a5c318c811a3168e77db8d0b0caa146a3
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2023-06-27 13:11:40 +0200

    Update monmodule.c

--- monmodule.c
+++ monmodule.c
@@ -1,20 +1,20 @@
-#include <linux/module.h> 
-#include <linux/kernel.h> 
- 
-MODULE_LICENSE("GPL");  
-MODULE_AUTHOR("Stagiaire");  
-MODULE_DESCRIPTION("Module hello world");  
-MODULE_VERSION("Version 1.00");  
- 
-int init_module(void)  
-{  
-     printk(KERN_INFO "[Hello world] - La fonction init_module() est 
-appelée.\n"); 
-     return 0;  
-}  
- 
-void cleanup_module(void)  
-{  
-     printk(KERN_INFO "[Hello world] - La fonction cleanup_module() 
-est appelée.\n");  
-} 
+// Nom : monmodule.c
+// Sujet: Module de noyau Linux
+// Formation
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Stagiaire");
+MODULE_DESCRIPTION("Module de noyau Linux");
+MODULE_VERSION("Version 1");
+int init_module(void)
+{
+printk(KERN_INFO "Le module est chargé.\n");
+return(0);
+}
+void cleanup_module(void)
+{
+printk(KERN_INFO "Le module est déchargé.\n");
+}
